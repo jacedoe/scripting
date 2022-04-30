155fb3b041a2f0718509fca743b497eb1c3a7891
Jesus Acedo <58938064+jacedoe@users.noreply.github.com>
2022-04-30 19:44:34 +0200

    Add files via upload

---
+++ vboxoff.ps1
@@ -0,0 +1,19 @@
+﻿$vm=C:\'Program Files'\Oracle\VirtualBox\.\VBoxManage.exe 
+$vmr=C:\'Program Files'\Oracle\VirtualBox\.\VBoxManage.exe list runningvms
+
+Foreach($i in $vmr)
+
+{
+
+New-Object -TypeName psobject -Property @{
+
+    index= $vmr.indexof($i)
+
+    Value= $i } 
+   
+    C:\'Program Files'\Oracle\VirtualBox\.\VBoxManage.exe controlvm $i.split(' ')[0] poweroff
+
+    }
+    Write-Host "All Machines poweroff, See you later"
+
+
